caee02a3067d10df9fa93c0e34c97d1bd5a2d8dd
Ankit <akc9065@gmail.com>
2021-01-15 14:53:53 +0530

    frontend ready start from backend

--- server.js
+++ server.js
@@ -12,15 +12,25 @@ const port = 3000 || process.env.PORT
 
 // Assets
 app.use(express.static('public'))
+app.use(expressLayout)
+app.set('views', path.join(__dirname, 'resources/views'))
+app.set('view engine', 'ejs')
 
 // first route to home page
 app.get('/', (req, res) =>{
     res.render('home')
 })
+app.get('/cart', (req, res) =>{
+    res.render('customers/cart')
+})
 
-app.use(expressLayout)
-app.set('views', path.join(__dirname, 'resources/views'))
-app.set('view engine', 'ejs')
+app.get('/login', (req, res) =>{
+    res.render('auth/login')
+})
+app.get('/register', (req, res) =>{
+    res.render('auth/register')
+})
+
 
 
 app.listen(port, ()=>{
