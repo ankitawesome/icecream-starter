469229a1f3f9707a9ae955ecfde9863eb06f0b6a
Ankit <akc9065@gmail.com>
2021-03-22 11:38:10 +0530

    We are done creating dedicated controllers for each routes next start fetching pizza from DB

--- server.js
+++ server.js
@@ -16,20 +16,10 @@ app.use(expressLayout)
 app.set('views', path.join(__dirname, 'resources/views'))
 app.set('view engine', 'ejs')
 
-// first route to home page
-app.get('/', (req, res) =>{
-    res.render('home')
-})
-app.get('/cart', (req, res) =>{
-    res.render('customers/cart')
-})
 
-app.get('/login', (req, res) =>{
-    res.render('auth/login')
-})
-app.get('/register', (req, res) =>{
-    res.render('auth/register')
-})
+require('./routes/web')(app)
+
+
 
 
 
